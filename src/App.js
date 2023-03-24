499c77b6188127a8cc8bfeff37f2a4ab751f8e1e
Aditya Singh <as808780@gmail.com>
2023-03-24 19:11:57 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [modelLink, setModelLink] = useState("")
+  useEffect(() => {
+    try{
+    const gltfLink = window.location.href.split('?model=')[1]
+    setModelLink(gltfLink)
+    }catch(e){}
+  }, [])
+  return (
+    <div className="App">
+      <model-viewer
+        alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
+        src={modelLink}
+        ar
+        ar-modes="webxr scene-viewer quick-look"
+        seamless-poster
+        shadow-intensity="1"
+        camera-controls
+        enable-pan
+        style={{ width: "100%", height: "100%" }}
+      ></model-viewer>
+    </div>
+  );
+}
+
+export default App;
